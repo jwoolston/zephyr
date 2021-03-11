dab9f3f9f28392f46700e3f3e41b7c7f177bebb2
Jared Woolston <Jared.Woolston@jacobs.com>
2021-03-11 15:04:37 -0800

    Changes device tree instance cast from STMMC_TypeDef to MMC_TypeDef because the latter is undefined.

--- subsys/disk/disk_access_stm32_sdmmc.c
+++ subsys/disk/disk_access_stm32_sdmmc.c
@@ -408,7 +408,7 @@ static const struct soc_gpio_pinctrl sdmmc_pins_1[] =
 
 static struct stm32_sdmmc_priv stm32_sdmmc_priv_1 = {
 	.hsd = {
-		.Instance = (SDMMC_TypeDef *)DT_INST_REG_ADDR(0),
+		.Instance = (MMC_TypeDef *)DT_INST_REG_ADDR(0),
 	},
 #if DT_INST_NODE_HAS_PROP(0, cd_gpios)
 	.cd = {
